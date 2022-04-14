902937e6e0d58c12a66b043f1b6566007f550f30
felimera <feli041089@gmail.com>
2022-04-14 09:18:13 -0500

    Sentencia SELECT con Parámetros

--- src/com/anncode/amazonviewer/dao/MovieDAO.java
+++ src/com/anncode/amazonviewer/dao/MovieDAO.java
@@ -1,12 +1,6 @@
 package com.anncode.amazonviewer.dao;
 
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE;
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE_CREATOR;
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE_DURATION;
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE_GENRE;
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE_ID;
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE_TITLE;
-import static com.anncode.amazonviewer.db.DataBase.TMOVIE_YEAR;
+import static com.anncode.amazonviewer.db.DataBase.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -33,6 +27,7 @@ public interface MovieDAO extends IDBConnetion {
 						rs.getString(TMOVIE_CREATOR), Integer.valueOf(rs.getString(TMOVIE_DURATION)),
 						Short.valueOf(rs.getString(TMOVIE_YEAR)));
 				movie.setId(Integer.valueOf(rs.getString(TMOVIE_ID)));
+				movie.setViewed(this.getMovieViewed(preparedStatement, connection, Integer.valueOf(rs.getString(TMOVIE_ID))));
 				movies.add(movie);
 			}
 		} catch (SQLException sqlException) {
@@ -41,7 +36,30 @@ public interface MovieDAO extends IDBConnetion {
 		return movies;
 	}
 
-	private boolean getMovieViewed() {
-		return false;
+	private boolean getMovieViewed(PreparedStatement preparedStatement,Connection connection,int idMovie) {
+		boolean viewed=false;
+		String sql="SELECT * FROM "+TVIEWED+
+				" WHERE "+
+				TVIEWED_ID_MATERIAL+"= ? "+
+				" AND "+
+				TVIEWED_ID_ELEMENT+"= ? "+
+				" AND "+
+				TVIEWED_ID_USER+"= ? ";
+		ResultSet rs=null;
+		try {
+			preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setInt(1, ID_TMATERIALS[0]);
+			preparedStatement.setInt(2, idMovie);
+			preparedStatement.setInt(3,TUSER_IDUSUARIO);
+			
+			rs=preparedStatement.executeQuery();
+			
+			viewed=rs.next();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return viewed;
 	}
 }
